ee834e3daf1bbc35ebbf49b84118409f9e2f8061
Lennard <starsight07@proton.me>
2025-07-12 19:51:20 +0200

    Added a float and posit representation, also changed gitignore

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class CreateCircle(Scene):
         self.play(Create(circle))  # show the circle on screen
 
 
-class BinaryBlocks(Scene):
+class Formula(Scene):
     def construct(self):
         square = Square()
         square.set_fill(BLUE, opacity=0.3)
@@ -19,3 +19,136 @@ class BinaryBlocks(Scene):
         self.play(Create(number))
         square.shift(RIGHT)
         self.wait()
+
+
+def BinarySequence(str :str):
+    group = VGroup()
+    for i, bit in enumerate(str):
+        sq = Square()
+        sq.set_fill(BLUE, opacity=0.3)
+        sq.shift(RIGHT * 2*i)
+        txt = Text(bit).scale(0.5)
+        txt.move_to(sq.get_center())
+        group += sq 
+        group += txt
+    group.width = 13
+    group.scale_to_fit_width
+    group.to_edge(LEFT)
+    return group
+
+def CreateFloat(str : str):
+    group = VGroup()
+
+    #Creates Numbers and Sqaures for Number
+    for i, bit in enumerate(str):
+        sq = Square()
+        if i == 0:
+            sq.set_fill(RED, opacity=0.3)
+        elif 1 <= i < 6:
+            sq.set_fill(YELLOW, opacity=0.3)
+        elif 6 <= i:
+            sq.set_fill(BLUE, opacity=0.3)
+        sq.shift(RIGHT * 2*i)
+        txt = Text(bit).scale(0.5)
+        txt.move_to(sq.get_center())
+        group += sq 
+        group += txt
+    #Creates Text for 
+    sign = Text("sign-Bit",color=RED)
+    exponent = Text("exponent-Bits",color=YELLOW)
+    mantissa = Text("mantissa-Bits",color=BLUE)
+    group.to_edge(LEFT)
+    #arranges sign to right place
+    sign.shift(2*UP)
+    sign.to_edge(LEFT)
+    #arranges exponent to right place
+    exponent.shift(2*UP)
+    exponent.to_edge(LEFT)
+    exponent.shift(RIGHT*4.5)
+    #arranges mantissa to right place
+    mantissa.shift(2*UP)
+    mantissa.to_edge(LEFT)
+    mantissa.shift(RIGHT*1.2*len(str))
+    #adds text to VGroup
+    group += sign
+    group += exponent
+    group += mantissa
+    #scales to screen size
+    group.width = 13
+    group.scale_to_fit_width
+    group.to_edge(LEFT)
+    return group
+
+
+
+def CreatePosit(str : str,es_value : int):
+    group = VGroup()
+    #TODO correct es_value to fit posit standart
+    #Creates Numbers and Sqaures for Number
+    for i, bit in enumerate(str):
+        sq = Square()
+        if i == 0:
+            sq.set_fill(RED, opacity=0.3)
+        elif 1 <= i < 1+es_value :
+            sq.set_fill(GREEN, opacity=0.3)
+        elif 1+es_value <= i <  1+es_value+2:
+            sq.set_fill(YELLOW, opacity=0.3)
+        elif 1+es_value+2 <= i:
+            sq.set_fill(BLUE, opacity=0.3)
+        sq.shift(RIGHT * 2*i)
+        txt = Text(bit).scale(0.5)
+        txt.move_to(sq.get_center())
+        group += sq 
+        group += txt
+    #Creates Text for 
+    sign = Text("sign-Bit",color=RED)
+    regime = Text("regime-Bits",color=GREEN)
+    exponent = Text("exponent-Bits",color=YELLOW)
+    frac = Text("fraction-Bits",color=BLUE)
+    group.to_edge(LEFT)
+    #arranges sign to right place
+    sign.shift(2*UP)
+    sign.to_edge(LEFT)
+    #arranges regime to right place
+    regime.shift(2*UP)
+    regime.to_edge(LEFT)
+    regime.shift(RIGHT*3.5)
+    #arranges exponent to right place
+    exponent.shift(2*UP)
+    exponent.to_edge(LEFT)
+    exponent.shift(RIGHT*10.5)
+    #arranges frac to right place
+    frac.shift(2*UP)
+    frac.to_edge(LEFT)
+    frac.shift(RIGHT*1.2*len(str))
+    #adds text to VGroup
+    group += sign
+    group += regime
+    group += exponent
+    group += frac
+    #scales to screen size
+    group.width = 13
+    group.scale_to_fit_width
+    group.to_edge(LEFT)
+    return group
+
+
+
+class NewScence(Scene):
+    def construct(self):
+        binary_str =  "0100101100010100"
+        binary_str2 = "0100010101001010"
+        float_bin = CreateFloat(binary_str)
+        float_bin.shift(DOWN*2)
+        float_bin2 = CreatePosit(binary_str2,4)
+        intro = Text("Floats vs. Posits")
+        intro.scale(2)
+        intro.to_edge(UP)
+        self.play(
+            Create(float_bin),Create(float_bin2),Create(intro)
+            )
+        self.wait
+
+
+
+
