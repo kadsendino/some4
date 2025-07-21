92d676404c0ed9030f0684272074c501179f4c93
Lennard <starsight07@proton.me>
2025-07-22 00:09:06 +0200

    smaller changes to floats, added index list function

--- floats.py
+++ floats.py
@@ -58,11 +58,73 @@ def create_binary_radix(str: str, index: int):
     point.next_to(b1,buff=0.05).shift(DOWN*0.2)
     return [b1,point,b2],VGroup(b1,point,b2).center()
 
+def create_bin_index_list(ind : int,bias=0):
+    array = []
+    group = VGroup()
+    # adds index infront
+    sq = Rectangle(width=4,stroke_width=2)
+    sq.set_fill(BLUE, opacity=0.3)
+    txt = Text("index")
+    sq.shift(RIGHT * 2)
+    txt.move_to(sq.get_center())
+    group += sq 
+    group += txt
+    array.append((sq,txt))  
+    for i in range(ind):
+        sq = Square(stroke_width=2)
+        sq.set_fill(BLUE, opacity=0.3)
+        zahl = i - bias
+        if zahl >= 0:
+            exp = 2**zahl
+            txt = MathTex(f"{exp}")
+        else:
+            exp = 2**(abs(zahl))
+            txt = MathTex(r"\frac{1}{" + str(exp) + "}")
+        sq.shift(RIGHT * 2*(i+2.5))
+        txt.move_to(sq.get_center())
+        group += sq 
+        group += txt
+        array.append((sq,txt))  
+    group.width = 13
+    group.scale_to_fit_width
+    group.to_edge(LEFT)
+    group.shift(UP*2)
+    return array, group
+
+def create_bin_index_list2(ind : int,bias=0):
+    array = []
+    group = VGroup()
+    # adds index infront
+    txt = Text("index")
+    txt.shift(RIGHT * 2) 
+    group += txt
+    array.append(txt)  
+    for i in range(ind):
+        zahl = i - bias
+        if zahl >= 0:
+            exp = 2**zahl
+            txt = MathTex(f"{exp}")
+        else:
+            exp = 2**(abs(zahl))
+            txt = MathTex(r"\frac{1}{" + str(exp) + "}")
+        txt.shift(RIGHT * 2*(i+2.5))
+        group += txt
+        array.append(txt)  
+    group.width = 13
+    group.scale_to_fit_width
+    group.to_edge(LEFT)
+    group.shift(UP*2)
+    return array, group
+
+
 class Test(Scene):
     def construct(self):
         binary_str = "0100010101001010"
+        a = 10
         array,group = create_binary_radix(binary_str,5)
-        self.play(Create(group))
+        a1,g1 = create_bin_index_list2(a,3)
+        self.play(Create(g1))
+        #self.play(Create(group))
         self.wait(2)
         
 
@@ -104,16 +166,22 @@ class FixedPoint(Scene):
         bin_num2.next_to(bin_num1,buff=0.15)
         p1.next_to(bin_num1,buff=0.05).shift(DOWN*0.2)
         g1 = VGroup(bin_num1,p1,bin_num2).center()
-        self.play(Transform(point,g1),FadeOut(bin_num))
+        self.play(Transform(point,g1,replace_mobject_with_target_in_scene=True),FadeOut(bin_num))
         #arrow = always_redraw(lambda : Arrow(start= p1.get_center()+DOWN*2,end=p1.get_center()+0.5*DOWN,color=RED))
 
         self.wait(2)
 
         bin_str = "010101010101000001010101010110"
         lastgroup = g1
-        for i in range(20):
+        tmp_a,tmp_group = create_bin_index_list(30)
+        self.play(Uncreate(txt1),Create(tmp_group))
+        for i in range(1,30):
             array,group = create_binary_radix(bin_str,i)
-            self.play(Transform(lastgroup,group,replace_mobject_with_target_in_scene=True,run_time=0.1))
+            indexl1_array,indexl1_group = create_bin_index_list(30,bias=i)
+            self.play(Transform(lastgroup,group,replace_mobject_with_target_in_scene=True,run_time=0.1),
+                      Transform(tmp_group,indexl1_group,replace_mobject_with_target_in_scene=True,run_time=0.1)
+                      )
+            self.wait(0.3)
             lastgroup = group
 
         
