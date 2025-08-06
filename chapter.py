caad4a41fe756cc74217068cc6524e23e364f428
Maximilian <prietzem04@zedat.fu-berlin.de>
2025-08-06 08:42:59 +0200

    added chapter anim

---
+++ chapter.py
@@ -0,0 +1,64 @@
+from manim import *
+
+
+CHAPTER_COLOR = WHITE
+
+chapters = [
+    "1. What where Floats again?",
+    "2. How does Posits work?",
+    "3. Comparison Floats vs. Posits"
+]
+
+class TableOfContents(Scene):
+    def construct(self):
+        group = VGroup()
+        for i,text in enumerate(chapters):
+            chapter = Text(text, font_size=48, color=CHAPTER_COLOR).to_edge(UP).to_edge(LEFT)
+            chapter.shift(DOWN * 2).shift(DOWN * i)
+            group.add(chapter)
+
+            self.play(Write(chapter),run_time=1)
+            self.wait(1)
+
+        self.play(FadeOut(group))
+        self.wait(1)
+
+class ChapterTitle(Scene):
+    def construct(self):
+        for i,text in enumerate(chapters):
+            chapter = Text(text, font_size=48, color=CHAPTER_COLOR)
+
+            self.play(Write(chapter),run_time=1)
+            self.wait(1)
+
+            self.play(FadeOut(chapter))
+            self.wait(1)
+
+class ImageTest(Scene):
+    def construct(self):
+        img  = ImageMobject("").scale_to_fit_height(config.frame_height).scale(0.28).to_edge(LEFT)
+        img2 = ImageMobject("").scale_to_fit_height(config.frame_height).scale(0.28)
+        img3 = ImageMobject("").scale_to_fit_height(config.frame_height).scale(0.28).to_edge(RIGHT)
+
+
+        padding = 0.075
+        img_rect = RoundedRectangle(width=img.width+padding,height=img.height+padding,corner_radius=0.2,color=BLACK,stroke_width=12)
+        img_rect.move_to(img)
+        img_rect2 = RoundedRectangle(width=img.width+padding,height=img.height+padding,corner_radius=0.2,color=BLACK,stroke_width=12)
+        img_rect2.move_to(img2)
+        img_rect3 = RoundedRectangle(width=img.width+padding,height=img.height+padding,corner_radius=0.2,color=BLACK,stroke_width=12)
+        img_rect3.move_to(img3)
+
+        img_text = Text("floats",font_size=32,color=CHAPTER_COLOR).next_to(img,UP)
+        img_text2 = Text("posits",font_size=32,color=CHAPTER_COLOR).next_to(img2,UP)
+        img_text3 = Text("comparison",font_size=32,color=CHAPTER_COLOR).next_to(img3,UP)
+
+
+        self.play(FadeIn(img),FadeIn(img_rect),FadeIn(img_text))
+        self.play(FadeIn(img2),FadeIn(img_rect2),FadeIn(img_text2))
+        self.play(FadeIn(img3),FadeIn(img_rect3),FadeIn(img_text3))
+        self.wait(1)
+        self.play(FadeOut(img),FadeOut(img_rect),FadeOut(img_text),FadeOut(img2),FadeOut(img_rect2),FadeOut(img_text2),FadeOut(img3),FadeOut(img_rect3),FadeOut(img_text3))
+        # self.play(FadeOut(img2),FadeOut(img_rect2),FadeOut(img_text2))
+        # self.play(FadeOut(img3),FadeOut(img_rect3),FadeOut(img_text3))
+        self.wait(1)
