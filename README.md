04e1b79f2cf351988c053716e3593ac5c329ba7d
Maximilian <prietzem04@zedat.fu-berlin.de>
2025-08-11 13:51:40 +0200

    added closure-plots

--- README.md
+++ README.md
@@ -1,66 +1,7 @@
-# Some4
+## Closure plots
 
-Animations for Some4
+Currently, only static images are implemented.
 
-## Installation
+Run `cargo run --release --bin image` in your terminal to generate some images.
 
-You have to install `python-uv` and `git`
-
-Clone repository and install manim:
-
-```
-git clone https://github.com/kadsendino/some4.git
-cd some4
-uv add manim
-```
-
-Run manim example scene `CreateCircle` from `main.py`:
-
-```
-uv run manim -pql main.py CreateCircle
-```
-
-## Voiceover
-
-For using voiceover run:
-
-```
-uv add gtts
-uv run gtts voiceover_test.py
-```
-
-For add it to animation:
-
-```
-uv add pydub
-uv run manim -pql voiceover_specialcases.py SpecialCases
-```
-
-## Contribution
-
-To create scenes please use the `main` branch but different files than `main.py`. In the end the finished scenes will be combined in `main.py`.
-
-## Documentation
-
-For documentation please visit the [Manim Community Website](https://docs.manim.community).
-
-
-## Images
-
-"beatingfloatspaper.png" [1]
-
-"Atari_1040STf_v2.jpg" [3]
-
-
-## References
-
-[1] Gustafson, J. L., & Yonemoto, I. T. (2017). Beating Floating Point at its Own Game: Posit Arithmetic. *Supercomputing Frontiers and Innovations*, 4(2), 71–86. https://doi.org/10.14529/jsfi170206  
-
-[2] Y. Uguen, L. Forget and F. de Dinechin, "Evaluating the Hardware Cost of the Posit Number System," *2019 29th International Conference on Field Programmable Logic and Applications (FPL)*, Barcelona, Spain, 2019, pp. 106-113, doi: 10.1109/FPL.2019.00026. keywords: {Standards;Hardware;Encoding;Libraries;Decoding;Open source software;Computer architecture;posit;architecture;fpga}  
-
-[3] Bill Bertram (2006), Atari 1040STf (1986), Own work, CC BY-SA 2.5, via Wikimedia Commons. https://de.wikipedia.org/wiki/Datei:Atari_1040STf.jpg  
-
-
-## License
-
-The written source code is licensed under [MIT License](https://github.com/kadsendino/some4?tab=MIT-1-ov-file).
+You can change the image file path, the resolution, closure operation and data types to be compared inside `src/bin/image.rs`.
---
+++ src/bin/image.rs
@@ -0,0 +1,168 @@
+use atomic_float::AtomicF64;
+use image::ExtendedColorType;
+use num_traits::Float;
+use half::f16;
+use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
+use std::sync::atomic::Ordering;
+use std::{fmt::Debug, iter::Iterator};
+use softposit::P16E1;
+
+fn main() {
+	let resolution = 14;
+	let image_res = 1 << resolution;
+
+	// 16-Bit floating points
+	let from_bits_f16 = |n: u16| {
+		// Floats start at 0, go up to maxReal, then continue with -0 and go down to minReal
+		// This orders the floats by size
+		let n = if n < (1 << 15) { !n } else { n - (1 << 15) };
+		f16::from_bits(n)
+	};
+
+	// Using addition
+	let operation = |x, y| x + y;
+	let operation_precise = |x: f64, y| x + y;
+
+	let (buf, min_err, max_err) = closure_plot(
+		resolution,
+		from_bits_f16,
+		operation,
+		operation_precise
+	);
+	dbg!(min_err, max_err);
+
+	let buf = image_buffer(buf, min_err, max_err);
+
+	image::save_buffer("addition_f16.png", &buf, image_res, image_res, ExtendedColorType::Rgb8)
+		.expect("Couldn't save image");
+
+
+
+	// 16-Bit Posits with es = 1
+	let from_bits_p16 = |n: u16| {
+		// Posits start at 0, go up to maxReal, then continue with minReal and go up
+		// This orders the posits by size
+		let n = n.wrapping_add(1 << 15);
+		P16E1::from_bits(n)
+	};
+
+	// Using addition
+	let operation = |x, y| x + y;
+	let operation_precise = |x: f64, y| x + y;
+
+	let (buf, min_err, max_err) = closure_plot(
+		resolution,
+		from_bits_p16,
+		operation,
+		operation_precise
+	);
+	dbg!(min_err, max_err);
+
+	let buf = image_buffer(buf, min_err, max_err);
+
+	image::save_buffer("addition_p16.png", &buf, image_res, image_res, ExtendedColorType::Rgb8)
+		.expect("Couldn't save image");
+
+}
+
+
+enum Accuracy {
+	Exact,
+	Inexact(f64), // Exact to how many decimals?
+	Overflow,
+	Underflow,
+	NotANumber
+}
+
+fn closure_plot<T, C, FB, OP, OPC>(
+	resolution: u8,
+	from_bits: FB,
+	operation: OP,
+	operation_precise: OPC
+) -> (Vec<Accuracy>, f64, f64)
+where
+	T: Float + Send + Sync, // Type we want to gerenate a closure plot for
+	C: Float + Send + Sync + From<T> + Into<f64> + Debug, // Second, more accurate type to compare T with
+	FB: (Fn(u16) -> T) + Send + Sync,
+	OP: (Fn(T, T) -> T) + Send + Sync,
+	OPC: (Fn(C, C) -> C) + Send + Sync, // Same operation but with C
+{
+	use Accuracy::*;
+
+	let min_err = AtomicF64::new(f64::INFINITY);
+	let max_err = AtomicF64::new(0.0);
+
+	(
+		(0u16..1 << resolution).into_par_iter().rev().flat_map(|x| {
+			let x = (1 << (16 - resolution)) * x;
+			(0u16..1 << resolution).map(|y| {
+				let y = (1 << (16 - resolution)) * y;
+
+				let x_t = from_bits(x);
+				let y_t = from_bits(y);
+				let x_c = <C as From<T>>::from(x_t);
+				let y_c = <C as From<T>>::from(y_t);
+
+				let result = operation(x_t, y_t);
+				let result = <C as From<T>>::from(result);
+				let result_precise = operation_precise(x_c, y_c);
+
+				let precision = -((result / result_precise).abs().log10().abs().log10());
+				// dbg!(precision);
+
+				// Special cases
+				if result.is_nan() {
+					NotANumber
+				} else if result.is_infinite() {
+					Overflow
+				} else if result == result_precise {
+					Exact
+				} else if result.is_zero() && !result_precise.is_zero() {
+					Underflow
+				} else {
+					min_err.fetch_min(precision.into(), Ordering::Relaxed);
+					max_err.fetch_max(precision.into(), Ordering::Relaxed);
+					Inexact(precision.into())
+				}
+			}).collect::<Vec<Accuracy>>()
+		}).collect(),
+		min_err.load(Ordering::Relaxed),
+		max_err.load(Ordering::Relaxed),
+	)
+}
+
+const PALETTE: [[u8; 3]; 5] = [
+	[  0,   0,   0], // Exact
+	[ 54,  74, 255], // Inexact
+	[255,  40,  40], // Overflow
+	[ 75, 255,  61], // Underflow
+	[255, 211,  54], // Not a number
+];
+
+
+fn image_buffer(buf: Vec<Accuracy>, min_err: f64, max_err: f64) -> Vec<u8> {
+	buf.iter().flat_map(|x| {
+		color(x, min_err, max_err)
+	}).collect()
+}
+
+fn color(case: &Accuracy, min_err: f64, max_err: f64) -> [u8; 3] {
+	use Accuracy::*;
+	match case {
+		Exact => PALETTE[0],
+		Inexact(error) => {
+			let [r, g, b] = PALETTE[1];
+			let error_adj = (max_err - error) / (max_err - min_err);
+			// dbg!(error, error_adj);
+
+			let r = r as f64 * error_adj;
+			let g = g as f64 * error_adj;
+			let b = b as f64 * error_adj;
+
+			[r as u8, g as u8, b as u8]
+		},
+		Overflow => PALETTE[2],
+		Underflow => PALETTE[3],
+		NotANumber => PALETTE[4],
+	}
+}
